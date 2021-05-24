90dc5b9886213def575a6ff7b92b35be47049ac5
guy <guyklinger94@gmail.com>
2021-05-24 15:03:02 +0300

    rebasing sql

--- project/create_tabels.sql
+++ project/create_tabels.sql
@@ -18,50 +18,32 @@
 -- );
 -- GO
 
--- IF OBJECT_ID('dbo.event_log', 'U') IS NOT NULL
--- DROP TABLE dbo.event_log
+-- -- Create a new table called 'TableName' in schema 'SchemaName'
+-- -- Drop the table if it already exists
+-- IF OBJECT_ID('dbo.matches', 'U') IS NOT NULL
+-- DROP TABLE dbo.matches
 -- GO
-
--- CREATE TABLE dbo.event_log
--- ( 
---     game_id INTEGER NOT NULL PRIMARY KEY,
---     game_date DATE NOT NULL,
---     minute INTEGER NOT NULL,
---     event_description INTEGER NOT NULL 
--- );
--- GO
-
-
-
--- IF OBJECT_ID('dbo.past_matches', 'U') IS NOT NULL
--- DROP TABLE dbo.past_matches
--- GO
-
+-- -- Create the table in the specified schema
 -- CREATE TABLE dbo.matches
 -- (
---     stage INTEGER NOT NULL PRIMARY KEY,
---     match_date DATE NOT NULL,
---     hour [NVARCHAR](50)  NOT NULL,
---     home_team [NVARCHAR](50)  NOT NULL,
---     away_team [NVARCHAR](50)  NOT NULL,
---     court [NVARCHAR](50) NOT NULL,
---     result [NVARCHAR](50),
---     game_id INTEGER REFERENCES db.event_log (game_id)-- check for object insertion
+--     matchId INT NOT NULL PRIMARY KEY, -- primary key column
+--     matchDate DATE NOT NULL,
+--     matchHour TIME NOT NULL,
+--     hostTeam [NVARCHAR](50)  NOT NULL,
+--     guestTeam [NVARCHAR](50)  NOT NULL,
+--     staduim [NVARCHAR](50) NOT NULL,
+--     coachID [NVARCHAR](50) NOT NULL,
+--     score [NVARCHAR](50),
 -- );
 -- GO
 
--- IF OBJECT_ID('dbo.FavoritePlayers', 'U') IS NOT NULL
--- DROP TABLE dbo.FavoritePlayers
+-- -- Create a new table called 'TableName' in schema 'SchemaName'
+-- -- Drop the table if it already exists
+-- IF OBJECT_ID('dbo.eventLog', 'U') IS NOT NULL
+-- DROP TABLE dbo.eventLog
 -- GO
+-- -- Create the table in the specified schema
 
--- CREATE TABLE dbo.FavoritePlayers
--- (
---     user_id INTEGER NOT NULL PRIMARY KEY,
---     player_id INTEGER NOT NULL ,
--- );
--- GO
-
--- Create the table in the specified schema
 -- CREATE TABLE dbo.eventLog
 -- (   
 --     matchId INT NOT NULL,    
@@ -73,6 +55,9 @@
 --     CONSTRAINT PK_matchId_hour PRIMARY KEY (matchId, eventHour)
 -- );
 -- GO
+
+
+------------------- INSERTIONS---------------------
 
 -- INSERT INTO dbo.matches (matchId, matchDate , matchHour , hostTeam , guestTeam , staduim ,coachID,score)
 -- VALUES (27, '10/07/2021', '19:00', 'Atletico madrid', 'Paris Saint-Germain', 'Metropolitano',6, '0-3');
@@ -91,4 +76,5 @@
 -- ALTER TABLE dbo.matches
 -- Change "column 1" "column 2" ["Data Type"];
 
-EXEC sp_rename 'dbo.matches.coachID', 'refereeID', 'COLUMN';
+-- EXEC sp_rename 'dbo.matches.coachID', 'refereeID', 'COLUMN';
+
