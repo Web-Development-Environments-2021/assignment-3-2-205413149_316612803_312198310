db01d35a1b31d20dc2b11327227ddd866a1640df
guy <guyklinger94@gmail.com>
2021-05-31 10:35:59 +0300

    u[dated team routes

--- project/routes/teams.js
+++ project/routes/teams.js
@@ -7,9 +7,9 @@ const teams_utils = require("./utils/teams_utils");
 router.get("/teamFullDetails/:teamId", async (req, res, next) => {
    let players_details = [];
   try {
-    // const players_details = await players_utils.getPlayersByTeam(
-    //   req.params.teamId
-    // );
+    const players_details = await players_utils.getPlayersByTeam(
+      req.params.teamId
+    );
 
     const teamName = await teams_utils.getTeamNameById(
       req.params.teamId
@@ -36,10 +36,13 @@ router.get("/teamFullDetails/:teamId", async (req, res, next) => {
 
 //route for search a team by name. returns partial data.
 router.get("/searchTeamByName/:teamName", async (req, res, next) => {
-  try{
-      let players = {};
-      players = await teams_utils.getTeamByName(req.params.teamName);
-      res.send(players);
+  try{      
+      const team = await teams_utils.getTeamByName(req.params.teamName);
+      if(team.length == 0){
+        throw {status: 404, message: "Bad Request. no such team."}
+      }
+      
+      res.send(team);
 
   } catch(error){
       next(error);
